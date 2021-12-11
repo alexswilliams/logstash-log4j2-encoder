f2e9f967eea2def6121208634f9f5b6a30420df4
Alex Williams <lexw@live.co.uk>
2021-12-11 18:18:01 +0000

    Change release process to move responsibility for java version onto releaser

--- release.sh
+++ release.sh
@@ -4,8 +4,11 @@
 
   set -ex
 
-  JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
-  export JAVA_HOME
+  javaMajorVersion=$(java -version 2>&1 | head -n 1 | sed -r 's/.*"([0-9]+\.[0-9]+)\..*/\1/')
+  if [[ ! $javaMajorVersion == "1.8" ]]; then
+    echo "Wrong version of java in use - detected $javaMajorVersion but required 1.8" >&2
+    exit 1
+  fi
 
   ./mvnw -Prelease release:clean clean package
 
